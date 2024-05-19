fe7fadc65090d81f44a772cf6a1fae564e87ecef
prolisbet <lizatrushina96@gmail.com>
2024-05-19 03:53:30 +0300

    first draft

--- main.py
+++ main.py
@@ -26,3 +26,58 @@
 # start(): начинает игру, чередует ходы игрока и компьютера, пока один из героев не умрет.
 # Выводит информацию о каждом ходе (кто атаковал и сколько здоровья осталось у противника)
 # и объявляет победителя.
+
+
+import random
+
+
+class Hero:
+    def __init__(self, name, health=100, attack_power=20):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
+    def attack(self, other):
+        other.health -= self.attack_power
+        print(f'Герой {self.name} атакует героя {other.name}.')
+        print(f'Здоровье героя {other.name}: {other.health}.')
+
+    def is_alive(self):
+        if self.health <= 0:
+            print(f'Герой {self.name} мертв.')
+            return False
+        return True
+
+
+class Game:
+    def __init__(self, player: Hero, computer: Hero):
+        self.player = player
+        self.computer = computer
+
+    def start(self):
+        print('Игра началась!')
+        choice = random.randint(0, 1)
+        if choice == 0:
+            first = self.computer
+            second = self.player
+            print(f'Первым действует герой {self.computer.name}.')
+        else:
+            first = self.player
+            second = self.computer
+            print(f'Первым действует герой {self.player.name}.')
+        while self.player.is_alive() and self.computer.is_alive():
+            first.attack(second)
+            second.attack(first)
+        print('Игра окончена.')
+        if self.player.is_alive():
+            print(f'Герой {self.player.name} победил!')
+        elif self.computer.is_alive():
+            print(f'Герой {self.computer.name} победил!')
+
+
+player1 = Hero('player')
+computer1 = Hero('computer')
+
+game = Game(player1, computer1)
+game.start()
+
